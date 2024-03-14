22be623352f23e1fab2872c1c4b96cc1a56e175e
Shashank Maurya <91915068+ShashankMaurya@users.noreply.github.com>
2024-03-14 17:13:23 +0530

    Update 4. TypeScript_demo.ts

--- 4. TypeScript_demo.ts
+++ 4. TypeScript_demo.ts
@@ -2,3 +2,19 @@ console.log("Hello world");
 
 // "tsc.filename.ts" to convert generate equivalent JS file
 // "node filename.js" to run JS file
+
+// -------------------------------------ONE-WAY DATA BINDING----------------------------------------
+// {{}} => String Interpolation
+//      => TS to HTML
+
+// [] => property binding
+//    => TS to HTML
+
+// () => Event Binding 
+//    => HTML to TS
+
+// -------------------------------------TWO-WAY DATA BINDING----------------------------------------
+// [()] =>  Binding 
+
+// -------------------------------------ANGULAR PIPES----------------------------------------
+// "Hello world" | uppercase
