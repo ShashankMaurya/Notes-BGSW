df2157de46a47824eb23a462cc8adec03fecf2d7
Shashank Maurya <91915068+ShashankMaurya@users.noreply.github.com>
2024-04-18 17:15:15 +0530

    Update REST_Flask.py

--- Python_Notes/REST_Flask.py
+++ Python_Notes/REST_Flask.py
@@ -1,6 +1,6 @@
 # pip install flask
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
@@ -17,6 +17,27 @@ def home():
 @app.route('/calc/<int:num>', methods=['GET'])
 def calc(num):
     return jsonify({'solution': num ** 2})
+
+
+# TODO: Made 2 endpoints, one for admin, and other for Standard user
+
+@app.route('/admin')
+def hello_admin():
+    return jsonify({'message': 'Hello Admin'})
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return jsonify({'message': 'Hello {} as Guest'.format(guest)})
+
+
+# TODO: Dynamic endpoint below, redirects to the respective endpoint based on the name, i.e., admin or guest.
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    return redirect(url_for('hello_guest', guest=name))
 
 
 if __name__ == '__main__':
